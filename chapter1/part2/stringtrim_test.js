446b40df006ca1d4466f56cee572c344f983fce7
Manuel Alonso <manuteali@gmail.com>
2013-01-04 13:46:08 +0100

    part2 , trim string

---
+++ chapter1/part2/stringtrim_test.js
@@ -0,0 +1,9 @@
+testCase("String trim test", {
+
+ "test trim should remove leading white-space": function () {
+	assert("should remove leading white-space", "a string" === "	a string".trim());
+  },
+ "test trim should remove trailing white-space": function () {
+	assert("should remove trailing white-space", "a string" === "a string    ".trim());
+  }
+});
